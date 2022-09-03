76ff8131a1d7009bad463e689583d75ff5251155
Matheus Rodrigues <luz.matheus11@gmail.com>
2022-09-03 17:09:26 -0300

    count numbers array

--- fundaments/bloco-4-javascript-logica-de-programacao/dia-2js-array-loop-For/index.js
+++ fundaments/bloco-4-javascript-logica-de-programacao/dia-2js-array-loop-For/index.js
@@ -1,5 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+let count = 0;
 for(let i = 0; i < numbers.length; i++) {
-  console.log(numbers[i]);
+  count += numbers[i];
 }
+console.log(count);
